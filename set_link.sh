35e570f6a63e2e6660efd6c82ba8290a712d2d77
Jiyong Jung <bwhite8129@gmail.com>
2013-12-14 20:44:55 +0900

    URL을 .io로 변경

--- set_link.sh
+++ set_link.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-LINK_ESC=${1//\//\\\/}
+LINK=http://vim-ko.github.io/releases/$1
+LINK_ESC=${LINK//\//\\\/}
 cp index.html index.html.tmp
 sed "s/id=\"release_link\".*/id=\"release_link\" href=\"$LINK_ESC\"/" < index.html.tmp > index.html
 rm index.html.tmp
